7da2179d7d66e06ba03fb8d6d001746b698f7054
aaaa727 <aaaa727@khu.ac.kr>
2020-03-09 19:42:08 +0900

    RData_3

---
+++ 03_1Variables/03_1.R
@@ -0,0 +1,52 @@
+a <- 1
+a
+
+b <- 2
+b
+
+c <- 3
+c
+
+d <- 3.5
+d
+
+a+b
+a+b+c
+4/b
+5*b
+
+var1 <- c(1,2,5,7,8)
+var1
+
+var2 <- c(1:5)
+var2
+
+var3 <- seq(1,5)
+var3
+
+var4 <-seq(1,10,by=2)
+var4
+
+var5 <-seq(1,10,by=3)
+var5
+
+var1+2
+
+var1+var2
+
+#문자로 된 변수 만들기
+
+str1 <-"a"
+str1
+
+str2 <- "text"
+str2
+
+str3 <- "Hello World"
+str3
+
+str4 <- c("a","b","c")
+str4
+
+str5 <- c("Hello","World","is","Good!")
+str5
---
+++ 03_1Variables/03_2.R
@@ -0,0 +1,30 @@
+#03_2
+
+#mean() average function
+
+x <- c(1,2,3)
+x
+
+mean(x)
+
+#max(),min() functions
+
+max(x)
+min(x)
+
+
+#character functions
+
+str5 <- c("Hello","World","is","good!")
+str5
+
+paste(str5,collapse = ",")
+paste(str5,collapse = " ")
+
+#3.New var
+
+x_mean <-mean(x)
+x_mean
+
+str5_paste <- paste(str5,collapse = " ")
+str5_paste
